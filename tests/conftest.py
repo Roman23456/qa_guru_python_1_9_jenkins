78a54c3be88c0bf6fb10d49b533129ed404f2a07
Roman Semenov <r_semenov@tochka.com>
2026-02-13 22:06:18 +0300

    UI test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,25 +2,25 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="function")
+# @pytest.fixture(scope="function")
+# def setup_browser():
+#     # options = Options()
+#     # # options.add_argument("--headless")
+#     # options.add_argument("--no-sandbox")
+#     # options.add_argument("--disable-dev-shm-usage")
+#     #
+#     # # Для локального запуска:
+#     # driver = webdriver.Chrome(options=options)
+
+
+@pytest.fixture(scope='function')
 def setup_browser():
     options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
 
-    # Для локального запуска:
-    driver = webdriver.Chrome(options=options)
-
-
-# @pytest.fixture(scope='function')
-# def setup_browser():
-#     options = Options()
-#
-#     driver = webdriver.Remote(
-#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
     # driver = webdriver.Chrome(options=options)
 
