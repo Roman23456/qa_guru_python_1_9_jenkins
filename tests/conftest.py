f467c5e0868195d997c8bc965e3c9b199d76f303
Roman Semenov <r_semenov@tochka.com>
2026-02-13 21:57:59 +0300

    UI test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def setup_browser():
     options = Options()
-    # options.add_argument("--headless")  # закомментируйте, чтобы видеть браузер
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
