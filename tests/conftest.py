1047ecc6638f97bc0e2b02d48122551b63e39112
Roman Semenov <r_semenov@tochka.com>
2026-02-13 21:51:22 +0300

    UI test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture(scope="function")
+def setup_browser():
+    options = Options()
+    # options.add_argument("--headless")  # закомментируйте, чтобы видеть браузер
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Для локального запуска:
+    driver = webdriver.Chrome(options=options)
+
+
+# @pytest.fixture(scope='function')
+# def setup_browser():
+#     options = Options()
+#
+#     driver = webdriver.Remote(
+#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+#         options=options
+#     )
+
+    # driver = webdriver.Chrome(options=options)
+
+    yield driver
