c4e65039d6e0796ff23941c71a1a3d7aa1d93e45
Roman Semenov <r_semenov@tochka.com>
2026-02-17 18:12:28 +0300

    h

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,8 +1,8 @@
 import allure
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
 
 class RegistrationPage:
@@ -10,7 +10,7 @@ class RegistrationPage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, timeout=10)
 
     @allure.step("Open url")
     def open(self):
@@ -18,39 +18,39 @@ class RegistrationPage:
 
     @allure.step('number')
     def fill_number(self, number):
-        element = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.ID, "input-number"))
+        element = WebDriverWait(self.driver, timeout=15).until(
+            EC.element_to_be_clickable((By.ID, "input-number"))
         )
         element.clear()
         element.send_keys(number)
 
     @allure.step('Text')
-    def fill_text(self,text):
-        element = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.ID, "input-text"))
+    def fill_text(self, text):
+        element = WebDriverWait(self.driver, timeout=15).until(
+            EC.element_to_be_clickable((By.ID, "input-text"))
         )
         element.clear()
         element.send_keys(text)
 
     @allure.step('Password')
     def fill_password(self, password):
-        element = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.ID, "input-password"))
+        element = WebDriverWait(self.driver, timeout=15).until(
+            EC.element_to_be_clickable((By.ID, "input-password"))
         )
         element.clear()
         element.send_keys(password)
 
     @allure.step('date')
     def fill_date(self, date):
-        element = WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.ID, "input-date"))
+        element = WebDriverWait(self.driver, timeout=15).until(
+            EC.element_to_be_clickable((By.ID, "input-date"))
         )
         element.clear()
         element.send_keys(date)
 
     @allure.step("Click Display Inputs button")
     def click_display_inputs(self):
-        element = WebDriverWait(self.driver, 10).until(
+        element = WebDriverWait(self.driver, timeout=10).until(
             EC.element_to_be_clickable((By.ID, "btn-display-inputs"))
         )
         element.click()
