0d677e674b2ab0d7af18961f73fc48b2d8e08574
Roman Semenov <r_semenov@tochka.com>
2026-02-17 19:11:52 +0300

    h

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,40 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from utils import attach
+
+from utils.attach import add_screenshot, add_page_source, add_console_logs, add_video
 
 
 @pytest.fixture(scope='function')
 def setup_browser():
     options = Options()
 
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
-    #driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(options=options)
 
     yield driver
 
-    attach.add_html(driver)
-    attach.add_screenshot(driver)
-    attach.add_logs(driver)
+    add_screenshot(driver)
+    add_page_source(driver)
+    add_console_logs(driver)
+    add_video(driver)
+
+    driver.quit()
 
 
 
--- utils/attach.py
+++ utils/attach.py
@@ -1,17 +1,25 @@
 import allure
 from allure_commons.types import AttachmentType
 
-# Скриншоты
-def add_screenshot(browser):
-    png = browser.driver.get_screenshot_as_png()
+
+def add_screenshot(driver):
+    png = driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
-# логи
-def add_logs(browser):
-    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+
+def add_console_logs(driver):
+    log = "".join(f'{text}\n' for text in driver.execute("getLog", {'type': 'browser'})['value'])
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
 
-# html-код страницы
-def add_html(browser):
-    html = browser.driver.page_source
+
+def add_page_source(driver):
+    html = driver.page_source
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+
+
+def add_video(driver):
+    video_url = "https://selenoid.autotests.cloud/video/" + driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + driver.session_id, AttachmentType.HTML, '.html')
