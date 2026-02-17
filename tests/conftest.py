29aca6dce65a3d0a8f66f0175394193ede084580
Roman Semenov <r_semenov@tochka.com>
2026-02-17 18:44:30 +0300

    h

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,7 @@
 import pytest
-from allure_commons._allure import attach
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-# @pytest.fixture(scope="function")
-# def setup_browser():
-#     options = Options()
-#     # options.add_argument("--headless")
-#     options.add_argument("--no-sandbox")
-#     options.add_argument("--disable-dev-shm-usage")
-#
-#     # Для локального запуска:
-#     driver = webdriver.Chrome(options=options)
+from utils import attach
 
 
 @pytest.fixture(scope='function')
@@ -30,3 +20,6 @@ def setup_browser():
     attach.add_html(driver)
     attach.add_screenshot(driver)
     attach.add_logs(driver)
+
+
+
